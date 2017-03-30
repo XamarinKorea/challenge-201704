4ab7e38c24f6166ba7522832d623e2085e96e97e
Bonkook Koo <justinaus2k@gmail.com>
2017-03-30 22:40:11 +0900

    no message

---
+++ clg1704/clg1704/Collections/ObservableCollectionCustomized.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace clg1704
+{
+	public class ObservableCollectionCustomized<T> : ObservableCollection<T>
+	{
+
+		public void AddRange(IEnumerable<T> range)
+		{
+			const string RANGE = "range";
+
+			if (range == null)
+			{
+				throw new ArgumentNullException(RANGE);
+			}
+
+			var items = range.ToList();
+			int index = Items.Count;
+			foreach (T item in range)
+			{
+				Items.Add(item);
+			}
+
+			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, items, index));
+		}
+	}
+}
