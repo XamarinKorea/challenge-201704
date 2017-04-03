57696da3ea986f8f2fa3a767a9ad5b396ae4c75f
Bonkook Koo <justinaus2k@gmail.com>
2017-04-03 18:27:23 +0900

    no message

---
+++ clg1704/clg1704/DetailPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace clg1704
+{
+	public partial class DetailPage : ContentPage
+	{
+		public DetailPage( UserItem item )
+		{
+			InitializeComponent();
+
+			this.Title = item.NameFirst;
+
+			imageUser.Source = item.PictureLarge;
+		}
+	}
+}
