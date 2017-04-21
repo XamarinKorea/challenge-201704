979026a20db596163c32f2875a8981d938d3ef63
rtrue <rtrue@naver.cm>
2017-04-21 10:21:06 +0900

    무한 스크롤

---
+++ InfiniteScroll/InfiniteScroll.Android/Renderers/CustomFontLabelRenderer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using InfiniteScroll.Droid.Renderers;
+using Android.Graphics;
+
+
+[assembly: ExportRenderer(typeof(Label), typeof(CustomFontLabelRenderer))]
+namespace InfiniteScroll.Droid.Renderers
+{
+    class CustomFontLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            var fontFamily = e.NewElement.FontFamily?.ToLower();
+            if (fontFamily != null && (fontFamily.EndsWith(".otf") || fontFamily.EndsWith(".ttf")))
+            {
+                var lable = (TextView)Control;
+                lable.Typeface = Typeface.CreateFromAsset(Forms.Context.Assets, e.NewElement.FontFamily);
+            }
+        }
+    }
+}
---
+++ InfiniteScroll/InfiniteScroll/Views/UserList.xaml.cs
@@ -0,0 +1,65 @@
+﻿using InfiniteScroll.Models;
+using InfiniteScroll.ViewModels;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace InfiniteScroll.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserList : ContentPage
+    {
+        readonly UserListViewModel _vm;
+
+        public UserList()
+        {
+            InitializeComponent();
+
+            _vm = new ViewModels.UserListViewModel();
+
+            BindingContext = _vm;
+
+            userlist.ItemAppearing += async (sender, e) =>
+            {
+                if (_vm.isLoading || _vm.DataSource.Count == 0)
+                    return;
+
+                var itemunit = e.Item as UserInfo;
+
+                if (itemunit.LoginUsername.ToString() == _vm.DataSource[_vm.DataSource.Count - 1].LoginUsername.ToString())
+                {                    
+                    await _vm.LoadItems();
+                }
+
+            };
+
+            _vm.navigationVR = Navigation;
+
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        protected override async void OnAppearing()
+        {
+            if (_vm.isLoading || _vm.DataSource != null ) return;
+
+            await _vm.LoadItems();
+
+        }
+
+        void UserTap(object sender, ItemTappedEventArgs e) => ((ListView)sender).SelectedItem = null;
+
+        /* Event To Command로 처리
+        async void UserSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var userinfo = ((ListView)sender).SelectedItem as UserInfo;
+            if (userinfo == null) return;
+
+              await Task.Yield();
+
+              await Navigation.PushAsync(new UserDetail(userinfo), false);
+        }
+        */
+
+    }
+}
