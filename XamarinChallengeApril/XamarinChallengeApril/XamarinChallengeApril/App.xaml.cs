9afff66b66d179a48279ed77e4bde575d13052d9
ironelder <ironelder@geninetworks.com>
2017-04-24 13:40:17 +0900

    도전과제 초보가 봐도 가장 쉽게 ... 내가 초보라서...

---
+++ XamarinChallengeApril/XamarinChallengeApril/XamarinChallengeApril/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using XamarinChallengeApril.Views;
+
+namespace XamarinChallengeApril
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new NavigationPage(new XamarinChallengeApril.MainPage());
+            //MainPage = new XamarinChallengeApril.MainPage();
+            BindingContext = new UsersPage();
+            //MainPage = new XamarinChallengeApril.Views.UsersPage();
+            MainPage = new NavigationPage(new XamarinChallengeApril.Views.UsersPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ XamarinChallengeApril/XamarinChallengeApril/XamarinChallengeApril/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace XamarinChallengeApril.Models
+{
+    public class Person
+    {
+        public string Name { get; private set; }
+
+        public string ImageUrl { get; private set; }
+
+        public string PhoneNumber { get; private set; }
+
+        public Person(string name, string imageUrl, string phoneNumber)
+        {
+            Name = name;
+            ImageUrl = imageUrl;
+            PhoneNumber = phoneNumber;
+        }
+
+    }
+}
